1ecc1600de094b076f0d8de24e0cd374d773be8a
hhadleydalt <hadleydalt@gmail.com>
2024-05-08 10:22:33 -0400

    results generate when file is uploaded

--- backend/code/static/index.js
+++ backend/code/static/index.js
@@ -2,7 +2,6 @@ function fetch_test() {
     fetch('/fetchtest').then(
         response => response.json()
     ).then(function(data) {
-        console.log(data)
         document.getElementById("to_change").innerHTML = data['hello']
     })
 }
