a7b5bba6ea267411d94f10dd4a3f828c841d6161
Dave <113065709+Dv-song@users.noreply.github.com>
2024-05-08 10:01:21 -0400

    ck

---
+++ backend/code/hyperparameters.py
@@ -0,0 +1,48 @@
+
+"""
+Number of epochs.
+"""
+num_epochs = 50
+
+"""
+Learning Rate for Optimizer
+"""
+# learning_rate = 1e-4
+learning_rate = 1e-4
+
+# learning_rate = 0.1
+
+"""
+Momentum on the gradient (if you use a momentum-based optimizer)
+"""
+momentum = 0.01
+
+"""
+Resize image size for dataset type.
+"""
+img_size = 224
+
+"""
+Sample size for calculating the mean and standard deviation of the
+training data. This many images will be randomly seleted to be read
+into memory temporarily.
+"""
+preprocess_sample_size = 400
+
+"""
+Maximum number of weight files to save to checkpoint directory. If
+set to a number <= 0, then all weight files of every epoch will be
+saved. Otherwise, only the weights with highest accuracy will be saved.
+"""
+max_num_weights = 5
+
+"""
+Defines the number of training examples per batch.
+You don't need to modify this.
+"""
+batch_size = 10
+
+"""
+The number of image scene classes. Don't change this.
+"""
+num_classes = 15
