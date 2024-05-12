eb6032a05beae513b819e83bb419b093cefde076
hhadleydalt <hadleydalt@gmail.com>
2024-05-12 19:48:06 -0400

    hi

--- code/predictor.py
+++ code/predictor.py
@@ -38,21 +38,7 @@ def calc_prediction(path):
     awhile, we return that they're drowsy, but that's much more binary than a percentage drowsiness. Not very interesting, we should discuss
     '''
 
-    '''
-    QUESTION: What if the person were to just blink 3 times or something because they have something in their eye? Or if they are drowsy but just happen to have their eyes open during the frames in question? 
-    - What if we took the 3 images AROUND the beginning, 3 images AROUND the middle, and same for the end, and predicted for each of them, and then returned the average percentage of the 3 for each one (beginning, middle, end)
-    - That way, if the person just happens to blink 3 times, they won't read as drowsy because their averages will still be low 
-    - Example:
-    - Someone who is NOT drowsy but happens to blink in the 3 frames: 
-        - Beginning ((0+0+1)/3 = 0.33) 
-        - Middle ((0+0+1)/3 = 0.33) 
-        - End ((0+0+1)/3 = 0.33)
-    - versus someone who IS drowsy but doesn't happen to blink in any of the frames: 
-        - Beginning ((0.5+0.3+0.6)/3 = 0.47)
-        - Middle ((0.6+0.5+0.6) = 0.56)
-        - End ((0.5+0.3+0.6)/3 = 0.47)
-    - There might be flaws with this, idk
-    '''
+    # returns # of blinks over period of time 
 
     return False
 
